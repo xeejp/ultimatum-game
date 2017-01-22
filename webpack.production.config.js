5e5aa254810015c110f8bc162ece608948e28f0e
Ryohei HAYASHI <hayashi@xee.jp>
2017-01-22 20:34:04 +0900

    Change webpack config.

--- webpack.production.config.js
+++ webpack.production.config.js
@@ -1,4 +1,5 @@
 var path = require('path');
+var webpack = require('webpack');
 
 module.exports = {
   entry: {
@@ -16,6 +17,19 @@ module.exports = {
       loader: "babel"
     }]
   },
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify('production')
+    }),
+    new webpack.optimize.DedupePlugin(),
+    new webpack.optimize.UglifyJsPlugin({
+      compress: {
+        warnings: false
+      }
+    }),
+    new webpack.optimize.OccurenceOrderPlugin(),
+    new webpack.optimize.AggressiveMergingPlugin()
+  ],
   resolve: {
     root: [
       path.resolve('./')
